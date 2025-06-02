2aa76f7ebb471dea4c002d54864325fc594830b4
Martin-mendez <130391732+Martin-mendez@users.noreply.github.com>
2025-06-02 13:40:42 -0300

    Configuración inicial del proyecto con LibGDX Liftoff

--- core/src/main/java/com/miempresa/eclipse/MiJuegoPrincipal.java
+++ core/src/main/java/com/miempresa/eclipse/MiJuegoPrincipal.java
@@ -3,7 +3,7 @@ package com.miempresa.eclipse;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture; 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
