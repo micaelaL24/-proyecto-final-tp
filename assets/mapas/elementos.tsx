91763c04dda5499f371dfab3774ec72c2be40f1e
Martin-mendez <130391732+Martin-mendez@users.noreply.github.com>
2025-08-03 23:24:53 -0300

    Add files via upload

---
+++ assets/mapas/elementos.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="elementos" tilewidth="64" tileheight="64" tilecount="98" columns="14">
+ <image source="platformPack_tilesheet.png" width="896" height="448"/>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="17" y="17" width="30" height="30"/>
+   <object id="2" x="17" y="17" width="30" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="piedra" x="0" y="0" width="64" height="64"/>
+   <object id="2" type="piedra" x="0" y="0" width="64" height="64"/>
+   <object id="3" x="0" y="0" width="64" height="64"/>
+   <object id="7" x="0" y="0" width="64" height="64"/>
+   <object id="8" x="0" y="0" width="64" height="64"/>
+   <object id="9" x="0" y="0" width="64" height="64"/>
+   <object id="10" x="0" y="0" width="64" height="64"/>
+   <object id="11" x="0" y="0" width="64" height="64"/>
+   <object id="12" x="0" y="0" width="64" height="64"/>
+   <object id="13" x="0" y="0" width="64" height="64"/>
+   <object id="14" x="0" y="0" width="64" height="64"/>
+   <object id="15" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+</tileset>
